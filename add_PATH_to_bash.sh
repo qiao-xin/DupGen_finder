336d7e0b62b577d445dad57a5150696563260255
Xin Qiao <qiaoxinqx2011@126.com>
2025-07-22 15:19:17 +0800

    Upload a shell script

---
+++ add_PATH_to_bash.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Xin Qiao, Jan 14 2020
+
+chmod 750 DupGen_finder.pl DupGen_finder-unique.pl
+
+path=`pwd`
+echo -e "The current directory will be added to your \$PATH environmental variable\n\n>>> $path\n"
+sleep 1
+
+echo Setting the \$PATH variable in the \~\/.bashrc file
+echo -e "...\n"
+echo -e "\n# added by DupGen_finder" >> ~/.bashrc
+echo PATH=$path:\$PATH >> ~/.bashrc
+#sed -i '$a PATH='$path':\$PATH' ~/.bashrc
+sleep 1
+
+echo Loading the new \$PATH into the current shell session
+echo -e "...\n"
+#source ~/.bashrc
+sleep 1
+
+echo Done!
